e8e703964339ea7f248a77192a7f823b15519d6b
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2022-10-05 20:29:27 -0300

    Agregado de atributos/getters/setter a materia prima

--- src/main/java/logico/MateriaPrima.java
+++ src/main/java/logico/MateriaPrima.java
@@ -10,4 +10,25 @@ package logico;
  */
 public class MateriaPrima {
     
+    private String nombre;
+    private String descripcion;
+    private int tipo;
+    private double precio_unidad;
+
+    public MateriaPrima() {
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getDescripcion(){
+        return descripcion;
+    }
+    public int getTipo(){
+        return tipo;
+    }
+    public double getPrecioUnidad(){
+        return precio_unidad;
+    }
+    
+    
 }
