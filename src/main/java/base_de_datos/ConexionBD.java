12efc935ccd2e40e363b6f2c3862281c77fb4131
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2022-10-05 20:30:14 -0300

    Clase conexion implementada

--- src/main/java/base_de_datos/ConexionBD.java
+++ src/main/java/base_de_datos/ConexionBD.java
@@ -1,5 +1,32 @@
 package base_de_datos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
+    protected Connection conexion;
+    private final String JDBC_DRIVER = "com.mysql.jdbc.Driver"; // org.postgresql.Driver
+    private final String BD_URL = "jdbc:mysql://localhost:3306/ejemplo"; // jdbc:postgresql://localhost:{puerto}/ejemplo
+    
+    private final String USER = "root"; //Ese es el que yo tengo configurado
+    private final String PASS = "1234";
+    
+    public void conectar() throws Exception{
+        try {
+            conexion = DriverManager.getConnection(BD_URL,USER,PASS);
+            Class.forName(JDBC_DRIVER);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    public void cerrar() throws SQLException{
+        if(conexion!=null){
+            if(!conexion.isClosed()){
+                conexion.close();
+            }
+        }
+    }
+    
     
 }
