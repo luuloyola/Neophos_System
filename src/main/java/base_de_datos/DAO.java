6429a38536b58df6c1091764b3ac9f80fe91d2c6
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2022-10-06 16:27:28 -0300

    Atributo ConexionBD dentro de la interfaz DAO

--- src/main/java/base_de_datos/DAO.java
+++ src/main/java/base_de_datos/DAO.java
@@ -3,6 +3,7 @@ package base_de_datos;
 import java.util.List;
 
 public interface DAO <T>{
+    public static ConexionBD conexion = new ConexionBD();
     public void create(T object) throws Exception;
     public void update(T object) throws Exception;
     public void delete(T object) throws Exception;
