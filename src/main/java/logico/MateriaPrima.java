88d58c8337b62f6671d429fe3ed9dd3679abfcb6
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2022-10-05 21:59:36 -0300

    Agregado de setters en materia prima

--- src/main/java/logico/MateriaPrima.java
+++ src/main/java/logico/MateriaPrima.java
@@ -10,6 +10,7 @@ package logico;
  */
 public class MateriaPrima {
     
+    private int id;
     private String nombre;
     private String descripcion;
     private int tipo;
@@ -28,6 +29,25 @@ public class MateriaPrima {
     }
     public double getPrecioUnidad(){
         return precio_unidad;
+    }
+    public int getId(){
+        return id;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    public void setDescripcion(String descripcion){
+        this.descripcion=descripcion;
+    }
+    public void setTipo(int tipo){
+        this.tipo=tipo;
+    }
+    public void setPrecioUnidad(double precio){
+        precio_unidad = precio;
+    }
+    public void setId(int id){
+        this.id=id;
     }
     
     
