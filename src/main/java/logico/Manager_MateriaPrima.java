df41c9f5c4b538b6e86c71c10e5a16eeadca019e
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2022-10-06 16:28:41 -0300

    Posible implementacion para unir el stock con la creacion de la materia prima

--- src/main/java/logico/Manager_MateriaPrima.java
+++ src/main/java/logico/Manager_MateriaPrima.java
@@ -1,9 +1,26 @@
 package logico;
 
+import base_de_datos.DAOMateriaPrima;
+
 public class Manager_MateriaPrima {
     
-    public void cargarMateriaPrima(){
+    public DAOMateriaPrima daoMatPrima;
+
+    public Manager_MateriaPrima() {
+        daoMatPrima = new DAOMateriaPrima();
+    }
+    
+    public void cargarMateriaPrima() throws Exception{
         MateriaPrima materia_nueva = new MateriaPrima();
+        Manager_StockMateria stock = new Manager_StockMateria();
+        stock.cargarStockMateria(5,Deposito.getSinglentonInstance(0), materia_nueva);
+        daoMatPrima.create(materia_nueva);
+    }
+    public void modificarMateriaPrima(MateriaPrima materia) throws Exception{
+        daoMatPrima.update(materia);
+    }
+    public void eliminarMateriaPrima(MateriaPrima materia) throws Exception{
+        daoMatPrima.delete(materia);
     }
     
 }
