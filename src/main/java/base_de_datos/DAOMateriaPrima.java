0b6be8f6801f26b46c2012874194c6172815f23b
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2022-10-05 21:58:55 -0300

    Implementacion completa de DAO en materia prima

--- src/main/java/base_de_datos/DAOMateriaPrima.java
+++ src/main/java/base_de_datos/DAOMateriaPrima.java
@@ -1,29 +1,92 @@
 package base_de_datos;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import logico.MateriaPrima;
 
 public class DAOMateriaPrima implements DAO<MateriaPrima>{
     
     ConexionBD conexion;
-
     @Override
     public void create(MateriaPrima object) throws Exception {
-        
+        try {
+            conexion.conectar();
+            PreparedStatement st = conexion.getConexion()
+                    .prepareStatement("INSERT INTO MateriaPrima (nombre, descripcion, tipo, precio) VALUES (?,?,?,?)");
+            st.setString(1, object.getNombre());
+            st.setString(2, object.getDescripcion());
+            st.setInt(3, object.getTipo());
+            st.setDouble(4, object.getPrecioUnidad());
+            st.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally{
+            conexion.cerrar();
+        }
     }
 
     @Override
     public void update(MateriaPrima object) throws Exception {
+        try {
+            conexion.conectar();
+            PreparedStatement st = conexion.getConexion()
+                    .prepareStatement("UPDATE MateriaPrima set nombre = ?,descripcion = ?, tipo = ?, precio = ? where id = ?");
+            st.setString(1, object.getNombre());
+            st.setString(2, object.getDescripcion());
+            st.setInt(3, object.getTipo());
+            st.setDouble(4, object.getPrecioUnidad());
+            st.setDouble(5, object.getId());
+            st.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally{
+            conexion.cerrar();
+        }
     }
 
     @Override
     public void delete(MateriaPrima object) throws Exception {
+        try {
+            conexion.conectar();
+            PreparedStatement st = conexion.getConexion()
+                    .prepareStatement("DELETE FROM MateriaPrima where id = ?");
+            st.setDouble(1, object.getId());
+            st.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally{
+            conexion.cerrar();
+        }
     }
 
     @Override
     public List<MateriaPrima> findAll() throws Exception {
-        return null;
+        List<MateriaPrima> lista = null;
+        try {
+            conexion.conectar();
+            PreparedStatement st = conexion.getConexion()
+                    .prepareStatement("SELECT * FROM MateriaPrima");
+            lista = new ArrayList<>();
+            ResultSet rs = st.executeQuery();
+            while(rs.next()){
+                MateriaPrima mat = new MateriaPrima();
+                mat.setId(rs.getInt("id"));
+                mat.setNombre(rs.getString("nombre"));
+                mat.setDescripcion(rs.getString("descripcion"));
+                mat.setTipo(rs.getInt("tipo"));
+                mat.setPrecioUnidad(rs.getDouble("precio"));
+                lista.add(mat);
+            }
+            rs.close();
+            st.close();
+        } catch (Exception e) {
+            throw e;
+        } finally{
+            conexion.cerrar();
+        }
+        return lista;
     }
-    
     
 }
