a7e2ccda3ea7257361e8322b1cc76575ccef513d
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2022-10-08 15:04:26 -0300

    Actualizacion Manager Materia

--- src/main/java/logico/Manager_MateriaPrima.java
+++ src/main/java/logico/Manager_MateriaPrima.java
@@ -15,8 +15,8 @@ public class Manager_MateriaPrima {
     public void cargarMateriaPrima(String nombre, String descripcion, String tipo, double precio_unidad, int cant) throws Exception{
         MateriaPrima materia_nueva = new MateriaPrima(nombre,descripcion,tipo,precio_unidad);
         System.out.println("Se creo la materia prima");
-        Manager_StockMateria stock = new Manager_StockMateria();
-        stock.cargarStockMateria(cant,Deposito.getSinglentonInstance(0), materia_nueva);
+        /*Manager_StockMateria stock = new Manager_StockMateria();
+        stock.cargarStockMateria(cant,Deposito.getSinglentonInstance(0), materia_nueva);*/
         System.out.println("Se puso en la BD el stock");
         daoMatPrima.create(materia_nueva);
         System.out.println("Se puso en la BD la materia prima");
