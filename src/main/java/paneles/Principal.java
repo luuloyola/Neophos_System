c3ce1ab3bbdabe0161c1a18d97371d266110a56b
agusgonella <92447039+agusgonella@users.noreply.github.com>
2022-10-09 19:57:33 -0300

    consultatr a panel

--- src/main/java/paneles/Principal.java
+++ src/main/java/paneles/Principal.java
@@ -217,7 +217,7 @@ public class Principal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void consultar_ordebActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_consultar_ordebActionPerformed
-        //Go_to aqui
+        //go_to(ConsultarOrdenDeCompra);
     }//GEN-LAST:event_consultar_ordebActionPerformed
 
     private void cargar_materiaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cargar_materiaActionPerformed
