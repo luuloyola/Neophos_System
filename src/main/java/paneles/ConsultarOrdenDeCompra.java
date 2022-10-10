f54a62afbdea254f6c21e51498b4efe4ac57a705
agusgonella <92447039+agusgonella@users.noreply.github.com>
2022-10-09 21:17:01 -0300

    algunas cosas q habian quedado mal

--- src/main/java/paneles/ConsultarOrdenDeCompra.java
+++ src/main/java/paneles/ConsultarOrdenDeCompra.java
@@ -119,9 +119,7 @@ public class ConsultarOrdenDeCompra extends javax.swing.JPanel {
                 .addComponent(Titulo2, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
                 .addContainerGap())
         );
-
-        mesas.setFont(new java.awt.Font("DialogInput", 1, 28));
-
+        
         Titulo.setFont(new java.awt.Font("Microsoft YaHei", 1, 21)); // NOI18N
         Titulo.setForeground(new java.awt.Color(97, 34, 34));
         Titulo.setText("CONSULTAR ORDEN DE COMPRA");
@@ -293,8 +291,6 @@ public class ConsultarOrdenDeCompra extends javax.swing.JPanel {
                     .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGap(0, 538, Short.MAX_VALUE)))
         );
-
-        mesas.setFont(new java.awt.Font("DialogInput", 1, 28));
 
         contenedor.add(inicio, "card2");
 
