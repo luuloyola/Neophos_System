2374de4f02eac68bb02bae9c389f0303817098e6
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2022-10-10 19:10:22 -0300

    Actualizacion Manager Materia Prima

--- src/main/java/logico/Manager_MateriaPrima.java
+++ src/main/java/logico/Manager_MateriaPrima.java
@@ -1,9 +1,7 @@
 package logico;
 
 import base_de_datos.DAOMateriaPrima;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Manager_MateriaPrima {
     
@@ -13,18 +11,12 @@ public class Manager_MateriaPrima {
         daoMatPrima = new DAOMateriaPrima();
     }
     
-    public void cargarMateriaPrima(String nombre, String descripcion, String tipo, double precio_unidad, double cant) throws Exception{
-        MateriaPrima materia_nueva = new MateriaPrima(nombre,descripcion,tipo,precio_unidad);
-        System.out.println("El Sistema puso los datos en un objeto Materia Prima\n");
+    public void cargarMateriaPrima(String nombre, String descripcion, String tipo, double precio_unidad, double cant, int id) throws Exception{
+        MateriaPrima materia_nueva = new MateriaPrima(nombre,descripcion,tipo,precio_unidad,id);
         daoMatPrima.create(materia_nueva);
-        daoMatPrima.consulta(61);
-        System.out.println("El Sistema puso la materia prima en la BD\n");
-        
         
         Manager_StockMateria stock = new Manager_StockMateria();
-        System.out.println("El Sistema puso los datos en un objeto stock\n");
         stock.cargarStockMateria(cant,Deposito.getSinglentonInstance(0), materia_nueva);
-        System.out.println("El Sistema puso el stock en la BD\n");
     }
     
     public MateriaPrima consultarMateriaPrima(int id) throws Exception{
