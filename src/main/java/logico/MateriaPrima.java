122e074e294835745f81a1c0b4f2db876c89dd15
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2022-10-10 19:10:32 -0300

    Actualizacion Materia Prima

--- src/main/java/logico/MateriaPrima.java
+++ src/main/java/logico/MateriaPrima.java
@@ -6,17 +6,21 @@ public class MateriaPrima {
     private String descripcion;
     private TipoMat tipoMateriaPrima;
     private double precio_unidad;
+    private int id_proveedor;
 
-    public MateriaPrima( String nombre, String descripcion, String tipo, double precio_unidad) {
+    public MateriaPrima( String nombre, String descripcion, String tipo, double precio_unidad, int id) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         if("PRODUCTO_QUIMICO".equals(tipo))
         tipoMateriaPrima = TipoMat.PRODUCTO_QUIMICO;
         else tipoMateriaPrima = TipoMat.INSUMO;
         this.precio_unidad = precio_unidad;
+        this.id_proveedor = id;  
     }
-    
-    public MateriaPrima() { // Constructor sin parametros
+    public MateriaPrima(){}
+
+    public int getId_proveedor() {
+        return id_proveedor;
     }
     public String getNombre(){
         return nombre;
@@ -31,8 +35,10 @@ public class MateriaPrima {
     public TipoMat getTipoMateriaPrima() {
         return tipoMateriaPrima;
     }
-    
-    
+
+    public void setId_proveedor(int id_proveedor) {
+        this.id_proveedor = id_proveedor;
+    }
     public void setNombre(String nombre){
         this.nombre=nombre;
     }
