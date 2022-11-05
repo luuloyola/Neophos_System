a23ad741e8e2a03c1d707485cb773704a9d35fef
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2022-11-05 18:05:17 -0300

    Implementacion realizada de Manager_Producto

--- src/main/java/logico/Manager_Producto.java
+++ src/main/java/logico/Manager_Producto.java
@@ -1,8 +1,36 @@
 package logico;
 
+import base_de_datos.DAOProducto;
+import java.util.List;
+
 
 public class Manager_Producto {
     
-    // Todavia no implementado
+    private static Manager_Producto manager;
+    private DAOProducto dao;
+    
+    public Manager_Producto(){
+        dao = new DAOProducto();
+    }
+    
+    public static Manager_Producto getInstance() {
+        if (manager == null)
+            manager = new Manager_Producto();
+
+        return manager;
+    }
+    
+    public void generarProveedor(){
+        
+    }
+    
+    public Proveedor consultarProveedor(String nombre) throws Exception{
+        return null;
+    }
+    
+    public List<Producto> getAllProductos() throws Exception{
+        return dao.findAll();
+    }
+
     
 }
