ac88ca44d30d7ad0bb2bf24ebbe8f7a2f8986704
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2022-11-05 18:05:39 -0300

    Añadido de constructor vacio en Producto

--- src/main/java/logico/Producto.java
+++ src/main/java/logico/Producto.java
@@ -7,6 +7,10 @@ public class Producto {
     public Producto( String nombre) {
         this.nombre = nombre;
     }
+
+    public Producto() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
     public String getNombre(){
         return nombre;
